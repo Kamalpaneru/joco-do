4e354581c1d928c114ef18a711a9b7182b686681
kervin5 <kervinlvh@gmail.com>
2016-10-05 23:06:04 -0700

    Feature: Add links to edit and logout

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,6 +2,5 @@ $(document).on('turbolinks:load', function() {
   $('.close')
     .on('click', function() {
       $(this).closest('.message').transition('fade');
-      console.log("worked");
     });
 });
